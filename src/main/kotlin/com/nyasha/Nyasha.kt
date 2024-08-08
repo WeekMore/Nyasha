e155c7ec457b0769f42ccdb4b9ac4477b9aa56b6
yuxiangll <yuxiangll@foxmail.com>
2024-08-08 21:30:58 +0800

    Main

---
+++ src/main/kotlin/com/nyasha/Nyasha.kt
@@ -0,0 +1,15 @@
+package com.nyasha
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Nyasha : ModInitializer {
+    private val logger = LoggerFactory.getLogger("nyasha")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
